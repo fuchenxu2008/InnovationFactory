f48ea5fe733d6c6a0ad84ecaaf073c5e3048c72a
fuchenxu2008 <fuchenxu2008@163.com>
2019-03-30 14:19:19 +0800

    db auth

--- server/config/index.js
+++ server/config/index.js
@@ -8,7 +8,7 @@ const {
 
 module.exports = {
   port: process.env.PORT || 4896,
-  mongoURL: `mongodb://${username}:${password}@${host}:${port}/${database}`,
+  mongoURL: `mongodb://${username}:${password}@${host}:${port}/${database}?authSource=admin`,
   APPID: 'wxdb862090658cbe67',
   APPSECRET: '768bd38f02eca17cf805acf659497722',
 };
